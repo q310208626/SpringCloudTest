ff257bf5673991bfeb7a9df2c1b498a366ed12b3
q310208626 <15521385907@163.com>
2024-09-28 20:45:24 +0800

    add feign interface

---
+++ OrderService/src/main/java/com/order/service/IRemoteStockService.java
@@ -0,0 +1,12 @@
+package com.order.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="stock-service")
+public interface IRemoteStockService {
+
+    @GetMapping("/stock/addProductStock")
+    String addProductStock(@RequestParam("productId") int productId, @RequestParam("num") int num);
+}
